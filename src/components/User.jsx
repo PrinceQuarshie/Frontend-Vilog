445dcb447a67849840a3d7cdc7404a4abd0d7a48
PrinceQuarshie <prince.quarshie@amalitech.org>
2022-08-24 12:48:18 +0000

    refactroing layout

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,35 +1,41 @@
 import React from "react";
 import "../styles/User.scss";
+// import logo from '../assets/Amalitech-Ghana.png'
+
 function User() {
   return (
     <div className="container">
-        <img src="/" alt="" />
+      {/* <img src="" alt="Amalitech-Ghana" className="logo" /> */}
+      <div className="regform"><h1>Sign up Details</h1></div>
       <form>
-         <div className="wrapper">
-           <div className="box">
-            <input type="text" name="fname" id="fname"/>
-            <label>First Name</label> 
-           </div>
+        <div className="wrapper">
+          <label>First Name</label>
+          <input className="firstname" type="text" name="fname" id="fname" />
 
-           <div className="box">
-            <input type="text" name="fname" id="fname"/>
-            <label>Last Name</label> 
-           </div>
+          <label>Last Name</label>
+          <input className="lastname" type="text" name="fname" id="fname" />
 
-           <div className="box">
-            <input type="email" name="email" id="email"/>
-            <label>Email</label> 
-           </div>
+          <label>Email</label>
+          <input className="email" type="email" name="email" id="email" />
 
-           <div className="box">
-            <input type="tel" name="tel" id="tel"/>
-            <label>Phone</label> 
-           </div>
+          <label>Phone</label>
+          <input className="tel" type="tel" name="tel" id="tel" />
 
-         <input type="submit" value="submit"/>
+          <label>Company</label>
+          <input className="company" type="text" name="company" id="tel" />
 
-         </div>
+          <label>Host</label>
+          <select className="option" id="">
+            <option disabled="disabled" selected="selected">--Select Name</option>
+            <option>Name 1</option>
+            <option>Name 2</option>
+            <option>Name 3</option>
+            <option>Name 4</option>
+            <option>Name 5</option>
+          </select>
 
+          <input type="submit" value="submit" />
+        </div>
       </form>
     </div>
   );
