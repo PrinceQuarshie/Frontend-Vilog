c454cfe30bcd8d52aa5f7fac575c8ea7c7bac189
PrinceQuarshie <prince.quarshie@amalitech.org>
2022-08-24 15:09:47 +0000

    form layout

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -4,12 +4,57 @@ import "../styles/User.scss";
 
 function User() {
   return (
-    <div className="container">
-      {/* <img src="" alt="Amalitech-Ghana" className="logo" /> */}
-      <div className="regform"><h1>Sign up Details</h1></div>
-      <form>
-        <div className="wrapper">
-          <label>First Name</label>
+    <body>
+      <div className="container">
+        <div className="title">Sign In</div>
+        <form>
+          <div className="user-details">
+            <div className="input-box">
+              <span className="details">First Name</span>
+              <input type="text" placeholder="Enter your first" />
+            </div>
+            <div className="input-box">
+              <span className="details">Last Name</span>
+              <input type="text" placeholder="Enter your last" />
+            </div>
+            <div className="input-box">
+              <span className="details">Email</span>
+              <input type="text" placeholder="Enter your email" required />
+            </div>
+
+            <div className="input-box">
+              <span className="details">Phone</span>
+              <input type="text" placeholder="Enter your number" />
+            </div>
+            <div className="input-box">
+              <span className="details">Comapny</span>
+              <input type="text" placeholder="Enter company name" />
+            </div>
+            <div className="input-box">
+              <span className="details">Host</span>
+              <select className="option" id="">
+                <option disabled="disabled" selected="selected">
+                  --Select Host Name
+                </option>
+                <option>Name 1</option>
+                <option>Name 2</option>
+                <option>Name 3</option>
+                <option>Name 4</option>
+                <option>Name 5</option>
+              </select>
+            </div>
+          </div>
+          <input type="submit" value="submit" />
+        </form>
+      </div>
+    </body>
+  );
+}
+
+export default User;
+{
+  /* <div className="wrapper">
+          <label className="firstlabel">First Name</label>
           <input className="firstname" type="text" name="fname" id="fname" />
 
           <label>Last Name</label>
@@ -34,11 +79,5 @@ function User() {
             <option>Name 5</option>
           </select>
 
-          <input type="submit" value="submit" />
-        </div>
-      </form>
-    </div>
-  );
+          <input type="submit" value="submit" /> */
 }
-
-export default User;
